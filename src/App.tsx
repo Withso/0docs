a36e5bbe2839a89f2edf08d55ee46cf0dbac21b6
Arun Raj Kumar <248166764+iamarunrk@users.noreply.github.com>
2026-03-08 13:18:32 +0000

    Add docs UI scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,16 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import GettingStarted from "./pages/GettingStarted";
+import Installation from "./pages/Installation";
+import Configuration from "./pages/Configuration";
+import Architecture from "./pages/Architecture";
+import Components from "./pages/Components";
+import Theming from "./pages/Theming";
+import ApiReference from "./pages/ApiReference";
+import Changelog from "./pages/Changelog";
+import Examples from "./pages/Examples";
+import FAQ from "./pages/FAQ";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -16,7 +26,17 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/getting-started" element={<GettingStarted />} />
+          <Route path="/installation" element={<Installation />} />
+          <Route path="/configuration" element={<Configuration />} />
+          <Route path="/architecture" element={<Architecture />} />
+          <Route path="/components" element={<Components />} />
+          <Route path="/components/*" element={<Components />} />
+          <Route path="/theming" element={<Theming />} />
+          <Route path="/api" element={<ApiReference />} />
+          <Route path="/changelog" element={<Changelog />} />
+          <Route path="/examples" element={<Examples />} />
+          <Route path="/faq" element={<FAQ />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
