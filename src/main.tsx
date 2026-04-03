360bb350da7d733a913acc967f1aaade031d7585
Arun Raj Kumar <248166764+iamarunrk@users.noreply.github.com>
2026-04-03 10:24:37 +0000

    Consolidated design tokens

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { initPlatformTheme } from "./hooks/use-platform-theme";
 import App from "./App.tsx";
-import "./styles/fonts.css";
 import "./index.css";
-import "./styles/platform.css";
 
 // Apply theme before first paint to prevent flash
 initPlatformTheme();
