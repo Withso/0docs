8fd983df4746e814aa11bb9c8772f445ff4fdf19
Arun Raj Kumar <248166764+iamarunrk@users.noreply.github.com>
2026-04-28 15:49:06 +0000

    Loosen Page metadata type from Record to any

--- src/hooks/use-builder.ts
+++ src/hooks/use-builder.ts
@@ -11,7 +11,7 @@ export interface Page {
   version_id?: string | null;
   nav_group_id?: string | null;
   nav_title?: string | null;
-  metadata?: Record<string, any>;
+  metadata?: any;
 }
 
 export interface Section {
